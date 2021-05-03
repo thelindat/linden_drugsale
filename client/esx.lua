aab89a6ba9e0a74c1b798725a6073eaf09fcbe62
Linden <65407488+thelindat@users.noreply.github.com>
2021-05-03 22:56:23 +1000

    Use exm import instead of object | Fix spacing | Add wait before starting resource

--- client/esx.lua
+++ client/esx.lua
@@ -1,19 +1,19 @@
-ESX = nil
-TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+ESX = ExM
 
 RegisterNetEvent('esx:playerLoaded')
 AddEventHandler('esx:playerLoaded', function(xPlayer)
-  ESX.PlayerData = xPlayer
-  StartResource()
+	ESX.PlayerData = xPlayer
+	Citizen.Wait(1000)
+	StartResource()
 end)
 
 RegisterNetEvent('esx:onPlayerLogout')	-- Trigger this event when a player logs out to character selection
 AddEventHandler('esx:onPlayerLogout', function()
-  PlayerLoaded = false
-  ESX.PlayerData = {}
+	PlayerLoaded = false
+	ESX.PlayerData = {}
 end)
 
 RegisterNetEvent('esx:setJob')
 AddEventHandler('esx:setJob', function(job)
-  ESX.PlayerData.job = job
+	ESX.PlayerData.job = job
 end)
