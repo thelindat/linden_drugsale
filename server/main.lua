f7e55afaba5ecb73e32f5b8cd02bcd13a8d88c4e
Linden <65407488+thelindat@users.noreply.github.com>
2021-05-03 20:46:49 +1000

    Check if player has the drugs, then sell them based on price in config

---
+++ server/main.lua
@@ -0,0 +1,15 @@
+ESX = nil
+TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+
+RegisterNetEvent('linden_drugsale:sellDrugs')
+AddEventHandler('linden_drugsale:sellDrugs', function(item)
+	local xPlayer = ESX.GetPlayerFromId(source)
+	if xPlayer then
+		local xItem = xPlayer.getInventoryItem(item)
+		if xItem.count > 0 then
+			local price = Config.Drugs[item] * xItem.count
+			xPlayer.removeInventoryItem(item, xItem.count)
+			xPlayer.addAccountMoney('money', price)
+		end
+	end
+end)
